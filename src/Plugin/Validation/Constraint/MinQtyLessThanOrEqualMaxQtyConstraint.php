7053458ad34779d3f5db05a2cbbc655f243fd0ef
olafkarsten <olafkarsten@352745.no-reply.drupal.org>
2017-12-06 21:36:15 +0100

    Issue #2847809: Implement validation against min/max constraints in quantity methods of ProductBundleItem

---
+++ src/Plugin/Validation/Constraint/MinQtyLessThanOrEqualMaxQtyConstraint.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\commerce_product_bundle\Plugin\Validation\Constraint;
+
+use Drupal\Core\Entity\Plugin\Validation\Constraint\CompositeConstraintBase;
+
+
+/**
+ * Helps validating that the minimum qty value is less than or equal to the maximum qty.
+ *
+ * @Constraint(
+ *   id = "MinQtyLessThanOrEqualMaxQty",
+ *   label = @Translation("Checks that minimum quantity is less or equal than maximum quantity.", context = "Validation"),
+ *   type = "entity:product_bundle_item"
+ * )
+ */
+class MinQtyLessThanOrEqualMaxQtyConstraint extends CompositeConstraintBase {
+
+  /**
+   * Message that will be shown if validation fails.
+   *
+   * @var string
+   */
+  public $message = 'Minimum Quantity (%min) must be less than or equal to Maximum Quantity (%max).';
+
+  /**
+   * @inheritdoc
+   */
+  public function coversFields(){
+    return ['min_quantity', 'max_quantity'];
+  }
+
+}
---
+++ src/Plugin/Validation/Constraint/MinQtyLessThanOrEqualMaxQtyConstraintValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\commerce_product_bundle\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+
+/**
+ * Validates the MinQtyLessThanOrEqualMaxQty constraint.
+ */
+class MinQtyLessThanOrEqualMaxQtyConstraintValidator extends ConstraintValidator {
+
+  public function validate($entity, Constraint $constraint) {
+    if (!isset($entity)) {
+      return;
+    }
+    $max = $entity->getMaximumQuantity();
+    $min = $entity->getMinimumQuantity();
+
+    if($min > $max){
+      $this->context->buildViolation($constraint->message)
+        ->setParameter('%min', $min)
+        ->setParameter('%max', $max)
+        ->addViolation();
+    }
+  }
+}
