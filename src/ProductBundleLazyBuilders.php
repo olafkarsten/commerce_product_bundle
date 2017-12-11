3e70b7ae01b8f03237630774c8bc3abaa8100bee
Steve Oliver <steve@circatree.com>
2017-12-11 10:20:35 -0800

    Issue #2929527 by steveoliver: Update ProductBundleLazyBuilders with new add to cart form id handling.

--- src/ProductBundleLazyBuilders.php
+++ src/ProductBundleLazyBuilders.php
@@ -2,8 +2,9 @@
 
 namespace Drupal\commerce_product_bundle;
 
-use Drupal\Core\Entity\EntityFormBuilderInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormState;
 
 /**
  * Provides #lazy_builder callbacks.
@@ -18,23 +19,23 @@ class ProductBundleLazyBuilders {
   protected $entityTypeManager;
 
   /**
-   * The entity form builder.
+   * The form builder.
    *
-   * @var \Drupal\Core\Entity\EntityFormBuilderInterface
+   * @var \Drupal\Core\Form\FormBuilderInterface
    */
-  protected $entityFormBuilder;
+  protected $formBuilder;
 
   /**
    * Constructs a new ProductBundleLazyBuilders object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
-   * @param \Drupal\Core\Entity\EntityFormBuilderInterface $entity_form_builder
-   *   The entity form builder.
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The form builder.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, EntityFormBuilderInterface $entity_form_builder) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, FormBuilderInterface $form_builder) {
     $this->entityTypeManager = $entity_type_manager;
-    $this->entityFormBuilder = $entity_form_builder;
+    $this->formBuilder = $form_builder;
   }
 
   /**
@@ -49,6 +50,10 @@ class ProductBundleLazyBuilders {
    *
    * @return array
    *   A renderable array containing the add to cart form.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Core\Form\EnforcedResponseException
+   * @throws \Drupal\Core\Form\FormAjaxException
    */
   public function addToCartForm($product_bundle_id, $view_mode, $combine) {
     /** @var \Drupal\commerce_order\OrderItemStorageInterface $order_item_storage */
@@ -57,14 +62,22 @@ class ProductBundleLazyBuilders {
     /** @var \Drupal\commerce_product_bundle\Entity\BundleInterface $product_bundle */
     $product_bundle = $this->entityTypeManager->getStorage('commerce_product_bundle')->load($product_bundle_id);
     $order_item = $order_item_storage->createFromPurchasableEntity($product_bundle);
-    $form_state_additions = [
-      'product_bundle' => $product_bundle,
+    /** @var \Drupal\commerce_cart\Form\AddToCartFormInterface $form_object */
+    $form_object = $this->entityTypeManager->getFormObject('commerce_order_item', 'add_to_cart');
+    $form_object->setEntity($order_item);
+    // The default form id is based on the variation ID, but in this case the
+    // product bundle id is more reliable (the variation/selection might change
+    // between requests due to an availability change, for example).
+    $form_object->setFormId($form_object->getBaseFormId() . '_commerce_product_bundle_' . $product_bundle_id);
+    $form_state = (new FormState())->setFormState([
+     'product_bundle' => $product_bundle,
       'view_mode' => $view_mode,
       'settings' => [
         'combine' => $combine,
       ],
-    ];
-    return $this->entityFormBuilder->getForm($order_item, 'add_to_cart', $form_state_additions);
+    ]);
+
+    return $this->formBuilder->buildForm($form_object, $form_state);
   }
 
 }
