7902d954c27546a56fc29c377dc1e978d5d7e9f9
olafkarsten <olafkarsten@352745.no-reply.drupal.org>
2018-01-05 20:45:45 +0100

    Issue #2934708 by olafkarsten: Add a bundle specific formatter for the commerce price field

---
+++ src/Plugin/Field/FieldFormatter/BundlePriceCalculatedFormatter.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\commerce_product_bundle\Plugin\Field\FieldFormatter;
+
+use Drupal\commerce\Context;
+use Drupal\commerce_price\Plugin\Field\FieldFormatter\PriceCalculatedFormatter;
+use Drupal\commerce_product_bundle\Entity\BundleInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+
+/**
+ * Plugin implementation of the 'commerce_price_calculated' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "commerce_product_bundle_calculated",
+ *   label = @Translation("Calculated product bundle price"),
+ *   field_types = {
+ *     "commerce_price"
+ *   }
+ * )
+ */
+class BundlePriceCalculatedFormatter extends PriceCalculatedFormatter implements ContainerFactoryPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    /** @var \Drupal\commerce\PurchasableEntityInterface $purchasable_entity */
+    $purchasable_entity = $items->getEntity();
+
+    // We only handle product bundles. Let the commerce core calculated price
+    // formatter handle other purchasable entities.
+    if (!$purchasable_entity->getEntityType()->entityClassImplements(BundleInterface::class)) {
+      return parent::viewElements($items, $langcode);
+    }
+
+    $context = new Context($this->currentUser, $this->currentStore->getStore());
+    $elements = [];
+
+    // We need to run the logic, even if we have no valid price from the bundle
+    // entity itself. The bundle price resolver will calculate a price from the
+    // product bundle items.
+    for ($delta = $items->isEmpty() ? 0 : 1; $delta <= count($items); $delta = $delta + 1) {
+      $resolved_price = $this->chainPriceResolver->resolve($purchasable_entity, 1, $context);
+      $number = $resolved_price->getNumber();
+      $currency = $this->currencyStorage->load($resolved_price->getCurrencyCode());
+
+      $elements[$delta] = [
+        '#markup' => $this->numberFormatter->formatCurrency($number, $currency),
+        '#cache' => [
+          'tags' => $purchasable_entity->getCacheTags(),
+          'contexts' => Cache::mergeContexts($purchasable_entity->getCacheContexts(), [
+            'languages:' . LanguageInterface::TYPE_INTERFACE,
+            'country',
+          ]),
+        ],
+      ];
+    }
+
+    return $elements;
+  }
+
+}
