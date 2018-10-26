31156236cc35592f7a0d8719e03e8f7e90d8f8eb
olafkarsten <olafkarsten@352745.no-reply.drupal.org>
2018-10-26 21:15:03 +0200

    Issue #3003806 by Manthan.addweb, fotograafinge, olafkarsten: Error: Call to a member function load() on null

--- src/Plugin/Field/FieldFormatter/BundlePriceCalculatedFormatter.php
+++ src/Plugin/Field/FieldFormatter/BundlePriceCalculatedFormatter.php
@@ -8,7 +8,6 @@ use Drupal\commerce_product_bundle\Entity\BundleInterface;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Language\LanguageInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 
 /**
  * Plugin implementation of the 'commerce_price_calculated' formatter.
@@ -21,42 +20,46 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
  *   }
  * )
  */
-class BundlePriceCalculatedFormatter extends PriceCalculatedFormatter implements ContainerFactoryPluginInterface {
+class BundlePriceCalculatedFormatter extends PriceCalculatedFormatter {
 
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
     /** @var \Drupal\commerce\PurchasableEntityInterface $purchasable_entity */
     $purchasable_entity = $items->getEntity();
-
     // We only handle product bundles. Let the commerce core calculated price
     // formatter handle other purchasable entities.
     if (!$purchasable_entity->getEntityType()->entityClassImplements(BundleInterface::class)) {
       return parent::viewElements($items, $langcode);
     }
 
-    $context = new Context($this->currentUser, $this->currentStore->getStore());
-    $elements = [];
+    $context = new Context($this->currentUser, $this->currentStore->getStore(), NULL, [
+      'field_name' => $items->getName(),
+    ]);
 
     // We need to run the logic, even if we have no valid price from the bundle
     // entity itself. The bundle price resolver will calculate a price from the
     // product bundle items.
     for ($delta = $items->isEmpty() ? 0 : 1; $delta <= count($items); $delta = $delta + 1) {
       $resolved_price = $this->chainPriceResolver->resolve($purchasable_entity, 1, $context);
-      $number = $resolved_price->getNumber();
-      $currency = $this->currencyStorage->load($resolved_price->getCurrencyCode());
-
-      $elements[$delta] = [
-        '#markup' => $this->numberFormatter->formatCurrency($number, $currency),
-        '#cache' => [
-          'tags' => $purchasable_entity->getCacheTags(),
-          'contexts' => Cache::mergeContexts($purchasable_entity->getCacheContexts(), [
-            'languages:' . LanguageInterface::TYPE_INTERFACE,
-            'country',
-          ]),
-        ],
-      ];
+      if($resolved_price){
+        $number = $resolved_price->getNumber();
+        $currency_code = $resolved_price->getCurrencyCode();
+        $options = $this->getFormattingOptions();
+        $elements[$delta] = [
+          '#markup' => $this->currencyFormatter->format($number, $currency_code, $options),
+          '#cache' => [
+            'tags' => $purchasable_entity->getCacheTags(),
+            'contexts' => Cache::mergeContexts($purchasable_entity->getCacheContexts(), [
+              'languages:' . LanguageInterface::TYPE_INTERFACE,
+              'country',
+            ]),
+          ],
+        ];
+      }
     }
 
     return $elements;
